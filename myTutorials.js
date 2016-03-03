e3071b7eab9421987cd441fa04c7130567af6453
Daniel Niclas <dniclas@sbcglobal.net>
2016-03-03 11:45:25 -0800

    Initial commit

---
+++ myTutorials.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Daniel on 2/16/2016.
+ */
+
+var mookArray = [
+    {
+        "display": "MOOK JavaScript Tutorial",
+        "url": "http://www.w3schools.com/js/default.asp"
+    },
+    {
+        "display": "MOOK HTML Tutorial",
+        "url": "http://www.w3schools.com/html/default.asp"
+    },
+    {
+        "display": "MOOK CSS Tutorial",
+        "url": "http://www.w3schools.com/css/default.asp"
+    }
+];
+
+
+myFunction(mookArray);
